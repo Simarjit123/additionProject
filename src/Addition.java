de2330e33cacbe305e8dea1ebecc063ad880b183
Simarjit <simarjassal9@gmail.com>
2021-12-18 23:07:58 -0500

    operators

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		int num1 = 22;
+		int num2 = 44;
+		int sum = num1 + num2;
+
+		System.out.println("Addition of two numbers is:");
+		System.out.println("Sum of the integers = " + sum);
+
+	}
+
+}
